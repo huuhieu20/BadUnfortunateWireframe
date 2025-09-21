ac3d3de5769042faf3cda351174fb52aa340afee
huuhieu20 <thaihuuhieulop6.6@gmail.com>
2025-09-21 03:44:05 +0000

    đf

--- main.py
+++ main.py
@@ -20,19 +20,19 @@ option = st.radio("Chủ đề", ["Dragon Ball", "Naruto", "One Piece"])
 # --- Dữ liệu mô hình ---
 models = {
     "Dragon Ball": [
-        {"id": "DB01", "name": "Goku", "img": "https://i.imgur.com/OUq8H0z.png"},
-        {"id": "DB02", "name": "Vegeta", "img": "https://i.imgur.com/pzXy7eJ.png"},
-        {"id": "DB03", "name": "Gohan", "img": "https://i.imgur.com/43QqfT7.png"}
+        {"id": "DB01", "name": "Goku", "img": "https://w7.pngwing.com/pngs/46/38/png-transparent-goku-thumbnail.png"},
+        {"id": "DB02", "name": "Vegeta", "img": "https://w7.pngwing.com/pngs/92/90/png-transparent-dragon-ball-z-vegeta-goku-gohan-dragon-ball-z-goku-child-gohan-thumbnail.png"},
+        {"id": "DB03", "name": "Gohan", "img": "https://w7.pngwing.com/pngs/302/315/png-transparent-gohan-goku-vegeta-dragon-ball-dragon-ball-cartoon-thumbnail.png"}
     ],
     "Naruto": [
-        {"id": "NA01", "name": "Naruto", "img": "https://i.imgur.com/oF0y8fZ.png"},
-        {"id": "NA02", "name": "Sasuke", "img": "https://i.imgur.com/3P1DOrC.png"},
-        {"id": "NA03", "name": "Kakashi", "img": "https://i.imgur.com/sUQNdwR.png"}
+        {"id": "NA01", "name": "Naruto", "img": "https://w7.pngwing.com/pngs/516/865/png-transparent-naruto-uzumaki-sasuke-uchiha-sakura-haruno-kakashi-hatake-naruto-cartoon-fictional-character-thumbnail.png"},
+        {"id": "NA02", "name": "Sasuke", "img": "https://w7.pngwing.com/pngs/806/774/png-transparent-sasuke-uchiha-naruto-uzumaki-sakura-haruno-kakashi-hatake-sasuke-uchiha-naruto-hand-manga-thumbnail.png"},
+        {"id": "NA03", "name": "Kakashi", "img": "https://w7.pngwing.com/pngs/620/978/png-transparent-kakashi-hatake-naruto-uzumaki-sasuke-uchiha-sakura-haruno-naruto-cartoon-comic-book-fictional-character-thumbnail.png"}
     ],
     "One Piece": [
-        {"id": "OP01", "name": "Luffy", "img": "https://i.imgur.com/hncvH5o.png"},
-        {"id": "OP02", "name": "Zoro", "img": "https://i.imgur.com/rV8pp3M.png"},
-        {"id": "OP03", "name": "Nami", "img": "https://i.imgur.com/7gQzZqv.png"}
+        {"id": "OP01", "name": "Luffy", "img": "https://w7.pngwing.com/pngs/101/513/png-transparent-monkey-d-luffy-nami-roronoa-zoro-one-piece-pirate-warriors-3-luffy-png-material-computer-wallpaper-cartoon-thumbnail.png"},
+        {"id": "OP02", "name": "Zoro", "img": "https://w7.pngwing.com/pngs/731/431/png-transparent-roronoa-zoro-monkey-d-luffy-one-piece-world-seeker-one-piece-treasure-cruise-zoro-pirate-fictional-character-thumbnail.png"},
+        {"id": "OP03", "name": "Nami", "img": "https://w7.pngwing.com/pngs/644/518/png-transparent-nami-monkey-d-luffy-roronoa-zoro-one-piece-grand-battle-anime-fictional-character-cartoon-thumbnail.png"}
     ]
 }
 
@@ -65,3 +65,4 @@ if submit:
         st.write("Cảm ơn bạn đã mua hàng 🛍️")
     else:
         st.error("⚠️ Vui lòng nhập đầy đủ thông tin trước khi xác nhận.")
+
